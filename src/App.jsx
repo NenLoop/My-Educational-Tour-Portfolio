6293f0a38754d7d18168a3447309a018cfc0f6e1
NenLoop <166492336+NenLoop@users.noreply.github.com>
2025-12-15 15:54:51 +0800

    home and about template just finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -31,45 +31,45 @@ function App() {
   }
 
   return (
-    <div className="scroll-smooth">
-    <div className='fixed z-1 top-12 left-1/2 transform -translate-x-1/2 rounded-2xl bg-white shadow-md w-full md:w-auto '>
+    <>
+    <div className='fixed z-1 top-12 left-1/2 transform -translate-x-1/2 rounded-2xl border-gray-400 border-2 bg-black/12 backdrop-blur-2xl shadow-2xl w-full md:w-auto '>
       <div className="flex items-center justify-center space-x-12 px-6">
-        <p className="font-bold text-xl">Ren Louie Peralta</p>
+        <p className="font-bold text-xl text-neon-purple">Ren Louie Peralta</p>
         <div>
           <ul className="flex space-x-8 py-4 px-2">
           <li>
-              <button className="text-lg font-medium" onClick={() => scrollToSection('home')}>Home</button>
+              <button className="text-lg text-off-white cursor-pointer font-medium" onClick={() => scrollToSection('home')}>Home</button>
             </li>
             <li>
-              <button className="text-lg font-medium" onClick={() => scrollToSection('about')}>About</button>
+              <button className="text-lg text-off-white cursor-pointer font-medium" onClick={() => scrollToSection('about')}>About</button>
             </li>
             <li>
-              <button className="text-lg font-medium" onClick={() => scrollToSection('tour')}>Tour</button>
+              <button className="text-lg text-off-white cursor-pointer font-medium" onClick={() => scrollToSection('tour')}>Tour</button>
             </li>
             <li>
-              <button className="text-lg font-medium" onClick={() => scrollToSection('contact')}>Contact</button>
+              <button className="text-lg text-off-white cursor-pointer font-medium" onClick={() => scrollToSection('footer')}>Contact</button>
             </li>
             <li>
-              <a className="text-lg font-medium" href="#">Github</a>
+              <a className="text-lg text-off-white font-medium" target="_blank" href="https://github.com/NenLoop">Github</a>
             </li>
           </ul>
         </div>
       </div>
     </div>
-    <div id="home" className="h-screen pt-16 relative flex items-center scroll-smooth">
+    <div id="home" className="h-screen pt-16 relative flex items-center">
       <div className="h-screen absolute inset-0" id="vanta"></div>
       <div className="relative mx-auto max-w-2xl">
         <div className="text-center">
-          <h1 className="text-7xl text-amber-600 font-bold">Ren Louie Peralta</h1>
-          <h2 className="text-5xl text-amber-600 font-bold mt-4">Self Taught Web Developer</h2>
-          <h3 className="text-2xl text-amber-600 font-medium mt-6">Davao City, Philippines</h3>
-          <h2 className="text-3xl text-amber-600 font-medium mt-4">I Specialized in sleeping</h2>
-          <div className="flex flex-row justify-center mt-4 space-x-6">
-            <button className="text-2xl p-4  bg-amber-600 font-medium mt-2">Contact Me</button>
-            <button className="text-2xl p-4 bg-amber-600 font-medium mt-2">My Contributions</button>
+          <h1 className="text-7xl text-neon-purple font-bold">Ren Louie Peralta</h1>
+          <h2 className="text-5xl text-soft-white font-semibold mt-6">Self Taught Web Developer</h2>
+          <h3 className="text-2xl text-amber-gold font-medium mt-6">Davao City, Philippines</h3>
+          {/* <h2 className="text-3xl text-off-white font-medium mt-4">I Specialized in sleeping</h2> */}
+          <div className="flex flex-row justify-center mt-6 space-x-8">
+            <button className="text-2xl px-6 py-4 bg-transparent border-2 border-magenta rounded-lg text-off-white font-medium cursor-pointer mt-2" onClick={() => scrollToSection('footer')}>Contact Me</button>
+            <button className="text-2xl px-6 py-4 bg-transparent border-2 border-magenta rounded-lg text-off-white font-medium cursor-pointer mt-2">My Contributions</button>
           </div>
-          <div>
-            <h1 className="text-2xl text-amber-600 font-medium mt-8"> Tech Stack</h1>
+          <div className='mt-4'>
+            <h1 className="text-2xl text-amber-gold font-medium mt-8">My Tech Stack</h1>
             <div className="flex flex-wrap space-x-8 justify-center">
               <div className='relative group inline-block  mt-8'>
                 <img src='/icons/javascript-logo-final.png' className='w-10 h-10'/>
@@ -113,7 +113,7 @@ function App() {
         </div>
       </div>
     </div>
-    <div id="about" className="min-h-screen bg-violet-black text-white scroll-mt-18 flex py-28 scroll-smooth">
+    <div id="about" className="min-h-screen bg-violet-black text-white scroll-mt-18 flex py-28">
       <div className="mx-auto w-full max-w-7xl px-6">
 
         {/* Main Content */}
@@ -130,22 +130,22 @@ function App() {
 
           {/* Text Content */}
           <div className="md:w-2/3 text-left">
-            <p className="text-4xl font-bold mb-4">
+            <p className="text-4xl text-white font-bold mb-4">
               Hello, I'm Ren Louie
             </p>
-            <p className="text-2xl font-semi mb-4">
+            <p className="text-2xl text-neon-purple font-semibold mb-4">
               Aspiring Network Engineer
             </p>
 
-            <p className="text-lg mb-4">
+            <p className="text-lg text-slate-gray mb-4">
               My journey into web development began with a curiosity for how websites are built and function. Over the years, I've honed my skills in various technologies, including JavaScript, Tailwind CSS, Java, Python, Django, and PostgreSQL.
             </p>
 
-            <p className="text-lg mb-4">
+            <p className="text-lg text-slate-gray mb-4">
               When I'm not coding, I enjoy exploring new technologies, contributing to open-source projects, and collaborating with fellow developers to create innovative solutions.
             </p>
 
-            <p className="text-lg mb-6">
+            <p className="text-lg text-slate-gray mb-6">
               Feel free to explore my portfolio and get in touch if you'd like to collaborate or learn more about my work!
             </p>
 
@@ -153,23 +153,23 @@ function App() {
           </div>
         </div>
         <div className="mx-auto max-w-4xl items-center flex flex-wrap justify-center mt-18 gap-12 text-sm text-gray-300 space-y-1">
-          <div className="bg-purple-600 w-48 py-8 text-center">
-            <p className='mb-2 text-xl'>20</p>
-            <p className='text-xl'>Years Old</p>
+          <div className="bg-obsidian-black rounded-xl w-48 py-8 text-center">
+            <p className='mb-2 text-xl text-amber-gold'>20</p>
+            <p className='text-xl text-slate-gray'>Years Old</p>
           </div>
-          <div className="bg-purple-600 w-48 py-8 text-center">
-            <p className='mb-2 text-xl'>Third</p>
-            <p className='text-xl'>Year Level</p>
+          <div className="bg-obsidian-black rounded-xl w-48 py-8 text-center">
+            <p className='mb-2 text-xl text-amber-gold'>Third</p>
+            <p className='text-xl text-slate-gray'>Year Level</p>
           </div>
-          <div className="bg-purple-600 w-48 py-8 text-center">
-            <p className='mb-2 text-xl'>BSIT</p>
-            <p className='text-xl'>Course Program</p>
+          <div className="bg-obsidian-black rounded-xl w-48 py-8 text-center">
+            <p className='mb-2 text-xl text-amber-gold'>BSIT</p>
+            <p className='text-xl text-slate-gray'>Course Program</p>
           </div>
         </div>
       </div>
     </div>
 
-    <div id="tour" className='h-screen scroll-smooth'>
+    <div id="tour" className='h-screen'>
       <div className='mx-auto max-w-xl items-center text-center py-10'>
         <h2 className="text-2xl font-medium inline">Tour Experience</h2>
         <div className="flex flex-wrap justify-center">
@@ -181,7 +181,7 @@ function App() {
       </div>
 
     </div>
-    <div id="contact" className='h-screen scroll-smooth'>
+    <div id="contact" className='h-screen'>
 
     </div>
     <div id="footer" className='max-h-48 overflow-hidden bg-black text-white text-center'>
@@ -193,7 +193,7 @@ function App() {
       <p>Footer</p>
     </div>
 
-    </div>
+    </>
 
   )
 }
